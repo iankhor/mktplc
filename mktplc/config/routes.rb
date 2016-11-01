4a74c9dbae846a07de37bbea0fdf656fcea307b2
jfhinchcliffe <hinchy@gmail.com>
2016-11-01 12:27:13 +1100

    Added items. Added my_items view for user

--- mktplc/config/routes.rb
+++ mktplc/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :items
   resources :profiles
   root 'static_pages#home'
+  get 'my_items', to: 'items#my_items'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- mktplc/db/schema.rb
+++ mktplc/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101003859) do
+ActiveRecord::Schema.define(version: 20161101010700) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
